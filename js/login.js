5131aee1b548d279e519a03e70a8e8b8e45172b5
Alexander Ruhl <ruhl-alex@web.de>
2026-03-31 20:18:38 +0200

    change login forwarding

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@
 
     const isAuthenticated = await authenticateUser(email, password);
     if (isAuthenticated) {
-        window.location.href = './board.html';
+        window.location.href = './summary.html';
     } else {
         showLoginError('Check your email and password. Please try again.');
     }
@@ -76,7 +76,7 @@ function showLoginError(message) {
 }
 
 function guestLogin() {
-    window.location.href = './board.html';
+    window.location.href = './summary.html';
 }
 
 function setupGuestLoginButton() {
