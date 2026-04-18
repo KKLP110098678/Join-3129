c0dcec7676f44185c2679111077f54b0859aa805
Alexander Ruhl <ruhl-alex@web.de>
2026-04-18 14:44:02 +0200

    update contact overlay for mobile devices

--- templates/contactsTemplate.js
+++ templates/contactsTemplate.js
@@ -1,7 +1,36 @@
 function showContactDetails(index, name, email, phone, initials, colorClass) {
     let detailContainer = document.getElementById('contact-detail-view');
 
-    detailContainer.innerHTML = `
+    if (window.innerWidth <= 768) {
+
+        detailContainer.innerHTML = `
+        <h3 class="info-headline">Contact Information</h3>
+        <div class="back-arrow" onclick="closeMobileDetails()"><img src="../assets/icon/sign/backarrow.svg" alt="Back"></div>
+        <div class="contact-header">
+            <div class="initials-big ${colorClass}">${initials}</div>
+            
+            <div class="name-section">
+                <h2>${name}</h2>
+                <div class="action-buttons">
+                   <span onclick="openEditContactOverlay(${index})">✏️ Edit</span>
+                <span onclick="deleteContact(${index})">🗑️ Delete</span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="contact-data">
+            
+            
+            <b>Email</b>
+            <a href="mailto:${email}">${email}</a>
+            
+            <b>Phone</b>
+            <span>${phone}</span>
+        </div>
+        `;
+    }
+    else {
+        detailContainer.innerHTML = `
         <div class="back-arrow" onclick="closeMobileDetails()">⬅️ Back</div>
         <div class="contact-header">
             <div class="initials-big ${colorClass}">${initials}</div>
@@ -25,7 +54,7 @@ function showContactDetails(index, name, email, phone, initials, colorClass) {
             <span>${phone}</span>
         </div>
     `;
-
+    }
     // Aktive Markierung setzen
     document.querySelectorAll('.contact-card').forEach(card => card.classList.remove('active-card'));
     let currentCard = document.getElementById('contactCard_' + index);
