ccdd08f0db5412ea47e4ad0b74e19c7d426b012b
Daniel <stuermer.dan137@gmail.com>
2026-04-29 17:34:54 +0200

    renamed summaryTemplate to boardCardTemplate // outsurced addTaskOverlay to seperate Template file // adjusted functionlatity // deleted old summary js file

---
+++ templates/addTaskOverlayTemplate.js
@@ -0,0 +1,123 @@
+function addTaskOverlayTemplate() {
+    return `
+        <div class="form-header">
+            <h2>Add Task</h2>
+            <button class="btn-light" id="btn-close" onclick="closeAddTaskForm()">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 13.4L7.09999 18.3C6.91665 18.4834 6.68332 18.575 6.39999 18.575C6.11665 18.575 5.88332 18.4834 5.69999 18.3C5.51665 18.1167 5.42499 17.8834 5.42499 17.6C5.42499 17.3167 5.51665 17.0834 5.69999 16.9L10.6 12L5.69999 7.10005C5.51665 6.91672 5.42499 6.68338 5.42499 6.40005C5.42499 6.11672 5.51665 5.88338 5.69999 5.70005C5.88332 5.51672 6.11665 5.42505 6.39999 5.42505C6.68332 5.42505 6.91665 5.51672 7.09999 5.70005L12 10.6L16.9 5.70005C17.0833 5.51672 17.3167 5.42505 17.6 5.42505C17.8833 5.42505 18.1167 5.51672 18.3 5.70005C18.4833 5.88338 18.575 6.11672 18.575 6.40005C18.575 6.68338 18.4833 6.91672 18.3 7.10005L13.4 12L18.3 16.9C18.4833 17.0834 18.575 17.3167 18.575 17.6C18.575 17.8834 18.4833 18.1167 18.3 18.3C18.1167 18.4834 17.8833 18.575 17.6 18.575C17.3167 18.575 17.0833 18.4834 16.9 18.3L12 13.4Z" fill="#4589FF"/>
+                </svg>
+            </button>
+        </div>
+        <div class="form-content">
+            <div class="form-group">
+                <input type="text" id="taskTitle" placeholder="Enter a title" onblur="validateTaskField('taskTitle')">
+                <output for="taskTitle">This field is required</output>
+            </div>
+            <div class="form-group">
+                <label for="taskDescription">
+                    Description
+                    <span class="optional">(optional)</span>
+                </label>
+                <textarea id="taskDescription" rows="3" placeholder="Enter a description"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="taskDueDate">Due Date</label>
+                <input type="date" id="taskDueDate" onblur="validateTaskField('taskDueDate')">
+                <output for="taskDueDate">This field is required</output>
+            </div>
+            <div class="form-group">
+                <p class="form-label">Priority</p>
+                <div class="radio-to-btn">
+                    <input type="radio" name="urgent-priority" class="d-none" id="urgentPriority" value="urgent">
+                    <label for="urgentPriority" class="priority-label urgent">
+                        Urgent
+                        <img src="../assets/icon/taskManagement/urgent.svg" alt="">
+                    </label>
+                    <input type="radio" name="urgent-priority" class="d-none" id="mediumPriority" value="medium">
+                    <label for="mediumPriority" class="priority-label medium">
+                        Medium
+                        <img src="../assets/icon/taskManagement/medium.svg" alt="">
+                    </label>
+                    <input type="radio" name="urgent-priority" class="d-none" id="lowPriority" value="low">
+                    <label for="lowPriority" class="priority-label low">
+                        Low
+                        <img src="../assets/icon/taskManagement/low.svg" alt="">
+                    </label>
+                </div>
+            </div>
+            <div class="form-group" id="assignedTo">
+                <label for="assignedToInput">
+                    Assigned To
+                    <span class="optional">(optional)</span>
+                </label>
+                <div class="custom-dropdown" id="assignedTo">
+                    <div class="dropdown-input-container">
+                        <input type="text" class="dropdown-input" id="assignedToInput"
+                            placeholder="Select contacts to assign"
+                            oninput="filterAssignedToDropdown(this.value)"
+                            onclick="toggleAssignedToDropdown(event)" />
+                        <button type="button" class="dropdown-toggle-btn" onclick="toggleAssignedToDropdown(event)">
+                            <img src="../assets/icon/task/dropdown-arrow.svg" alt="dropdown arrow" class="dropdown-arrow" />
+                        </button>
+                    </div>
+                    <div class="dropdown-list assigned-to d-none" id="assignedToDropdown"></div>
+                    <div class="assignees" id="assigneeIconsContainer"></div>
+                </div>
+            </div>
+            <div class="form-group" id="category-form-group">
+                <p class="form-label">Category</p>
+                <div class="custom-dropdown" id="category">
+                    <div class="dropdown-input-container">
+                        <input type="text" class="dropdown-input" id="categoryInput"
+                            placeholder="Select task category" readonly
+                            onclick="toggleCategoryDropdown(event)" required />
+                        <button type="button" class="dropdown-toggle-btn" onclick="toggleCategoryDropdown(event)">
+                            <img src="../assets/icon/task/dropdown-arrow.svg" alt="dropdown arrow" class="dropdown-arrow" />
+                        </button>
+                    </div>
+                    <div class="dropdown-list d-none" id="categoryDropdown">
+                        <div class="dropdown-item category" onclick="selectCategory('Technical Task')">
+                            <label>Technical Task</label>
+                        </div>
+                        <div class="dropdown-item category" onclick="selectCategory('User Story')">
+                            <label>User Story</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <p class="form-label">Subtasks <span class="optional">(optional)</span></p>
+                    <div id="subtasksContainer">
+                        <div class="subtask-input-group">
+                            <input type="text" class="subtask-input" id="subtaskInput"
+                                placeholder="Add new subtask" onfocus="showSubtaskInputButtons()">
+                            <div class="input-btn-group d-none" id="subtaskBtnGroup">
+                                <button type="button" class="input-btn" onclick="clearSubtaskInput()">
+                                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6.575 7.975L1.675 12.875C1.49167 13.0583 1.25833 13.15 0.975 13.15C0.691667 13.15 0.458333 13.0583 0.275 12.875C0.0916667 12.6917 0 12.4583 0 12.175C0 11.8917 0.0916667 11.6583 0.275 11.475L5.175 6.575L0.275 1.675C0.0916667 1.49167 0 1.25833 0 0.975C0 0.691667 0.0916667 0.458333 0.275 0.275C0.458333 0.0916667 0.691667 0 0.975 0C1.25833 0 1.49167 0.0916667 1.675 0.275L6.575 5.175L11.475 0.275C11.6583 0.0916667 11.8917 0 12.175 0C12.4583 0 12.6917 0.0916667 12.875 0.275C13.0583 0.458333 13.15 0.691667 13.15 0.975C13.15 1.25833 13.0583 1.49167 12.875 1.675L7.975 6.575L12.875 11.475C13.0583 11.6583 13.15 11.8917 13.15 12.175C13.15 12.4583 13.0583 12.6917 12.875 12.875C12.6917 13.0583 12.4583 13.15 12.175 13.15C11.8917 13.15 11.6583 13.0583 11.475 12.875L6.575 7.975Z" fill="#4589FF"/>
+                                    </svg>
+                                </button>
+                                <div class="input-btn-seperator"></div>
+                                <button type="button" class="input-btn" onclick="addSubtask()">
+                                    <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5.288 8.775L13.763 0.3C13.963 0.1 14.2005 0 14.4755 0C14.7505 0 14.988 0.1 15.188 0.3C15.388 0.5 15.488 0.7375 15.488 1.0125C15.488 1.2875 15.388 1.525 15.188 1.725L5.988 10.925C5.788 11.125 5.55467 11.225 5.288 11.225C5.02133 11.225 4.788 11.125 4.588 10.925L0.288 6.625C0.088 6.425 -0.00783333 6.1875 0.0005 5.9125C0.00883333 5.6375 0.113 5.4 0.313 5.2C0.513 5 0.7505 4.9 1.0255 4.9C1.3005 4.9 1.538 5 1.738 5.2L5.288 8.775Z" fill="#4589FF"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="subtask-list" id="subtaskList"></div>
+                </div>
+            </div>
+            <input type="hidden" id="taskStatus" value="todo">
+        </div>
+        <div class="form-buttons">
+            <button class="btn-clear btn-clear-mobile" onclick="clearForm()">Clear ✕</button>
+            <button class="btn-primary-with-icon" onclick="saveNewTask()">
+                Create Task
+                <svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.55 15.15L18.025 6.675C18.225 6.475 18.4625 6.375 18.7375 6.375C19.0125 6.375 19.25 6.475 19.45 6.675C19.65 6.875 19.75 7.1125 19.75 7.3875C19.75 7.6625 19.65 7.9 19.45 8.1L10.25 17.3C10.05 17.5 9.81667 17.6 9.55 17.6C9.28333 17.6 9.05 17.5 8.85 17.3L4.55 13C4.35 12.8 4.25417 12.5625 4.2625 12.2875C4.27083 12.0125 4.375 11.775 4.575 11.575C4.775 11.375 5.0125 11.275 5.2875 11.275C5.5625 11.275 5.8 11.375 6 11.575L9.55 15.15Z" fill="white"/>
+                </svg>
+            </button>
+        </div>
+    `;
+}
