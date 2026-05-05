7c8beb25e2ef29bd50dfc1bea12de700cf5d115d
Miggi97G <m.famgarcia@outlook.com>
2026-05-05 12:11:13 +0200

    git ignore

--- js/database/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  databaseURL:
-    "https://join3129-90b41-default-rtdb.europe-west1.firebasedatabase.app/",
-};
-
-const app = firebase.initializeApp(firebaseConfig);
-
-const db = firebase.database();
